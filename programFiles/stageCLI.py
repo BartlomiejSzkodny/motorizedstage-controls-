0b8c4aad7a6810a1bb5ad949c1a7456525bbffa3
BartlomiejSzkodny <szkobart@outlook.com>
2024-12-14 20:00:39 +0100

    forgot aboout files

---
+++ programFiles/stageCLI.py
@@ -0,0 +1,6 @@
+class StageCli:
+    def __init__(self):
+        print("StageCLI initialized")
+        print("Cli is now running")
+        print("Please enter a command")
+        print("Type help to see the list of commands")
