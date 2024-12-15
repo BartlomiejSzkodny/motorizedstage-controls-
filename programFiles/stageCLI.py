435aad20502c8185604f689a2cc82e1c62049b5b
Libi Rajzer <michal.rajzer03@gmail.com>
2024-12-16 00:19:23 +0100

    Added missing help

--- programFiles/stageCLI.py
+++ programFiles/stageCLI.py
@@ -1,6 +1,7 @@
 from programFiles.stage import Stage
 from serial.tools import list_ports
 
+
 class StageCli():
     def __init__(self):
         self.stage = Stage()
@@ -8,7 +9,7 @@ class StageCli():
         print("Cli is now running")
         print("Please enter a command")
         print("Type help to see the list of commands")
-    
+
     def run_command(self, command: str):
         if command == "help":
             print("*\n"*5)
@@ -17,8 +18,9 @@ class StageCli():
             print("velmove - moves the stage at a specific velocity")
             print("goto - moves the stage")
             print("exit - exits the program")
+            print("connect - connects to the stage")
 
-        if command=="connect":
+        if command == "connect":
             print("Available COM ports:")
             ports = list_ports.comports()
             if not ports:
@@ -29,16 +31,14 @@ class StageCli():
                 k = input("Enter the COM port: ")
                 self.stage.init_prior(ports[int(k)])
 
-
         if command == "velmove":
             x = input("Enter the x position: ")
             y = input("Enter the y position: ")
             speed = input("Enter the speed: ")
             self.stage.move_at_velocity(x, y, speed)
-        
+
         if command == "goto":
             x = input("Enter the x position: ")
             y = input("Enter the y position: ")
             speed = input("Enter the speed: ")
             self.stage.goto_position(x, y, speed)
-        
